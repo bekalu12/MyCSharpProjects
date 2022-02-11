295d2cea939e127a26926b6f1df8100ea607f046
bekalu12 <bekaluyirgalem85@gmail.com>
2022-02-11 07:35:07 -0800

    add files

---
+++ Test1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+        class Car
+        {
+            public string color = "red";
+        }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+        }
+    }
+}
